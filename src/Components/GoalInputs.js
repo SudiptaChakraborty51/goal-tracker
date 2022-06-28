0c7eec4a8f0b99e540aeffb458fc774d728f0949
Sudipta <hisudipta26@gmail.com>
2022-06-29 00:36:49 +0530

    add styling

--- src/Components/GoalInputs.js
+++ src/Components/GoalInputs.js
@@ -3,13 +3,21 @@ import './GoalInputs.css';
 
 function GoalInputs(props) {
     const [enteredGoalInput, setEnteredGoalInput] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = event => {
         setEnteredGoalInput(event.target.value);
+        if(event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
     }
 
     const formSubmitHandler = event =>  {
         event.preventDefault();
+        if(enteredGoalInput.trim().length === 0){
+            setIsValid(false);
+            return;
+        }
         props.onAddGoals(enteredGoalInput);
         setEnteredGoalInput('');
     }
@@ -17,8 +25,12 @@ function GoalInputs(props) {
     return (
         <form onSubmit={formSubmitHandler}>
             <div className="form-control">
-                <label>Goal</label>
-                <input type="text" value={enteredGoalInput} onChange={goalInputChangeHandler} />
+                <label style={{color: !isValid? '#dc2626' : 'black'}}>Goal</label>
+                <input type="text" value={enteredGoalInput} onChange={goalInputChangeHandler} 
+                       style={{borderColor: !isValid? '#dc2626' : '#bfdbfe', 
+                       backgroundColor: !isValid? '#fca5a5' : 'transparent'}} 
+                       placeholder='write your goal here'
+                />
             </div>
             <button type="submit" className="button">Add Goal</button>
         </form>
