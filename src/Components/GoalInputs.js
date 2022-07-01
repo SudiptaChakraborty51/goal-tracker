d159abb1357bcd83aae68d2a9036ed4c32fd2473
Sudipta <hisudipta26@gmail.com>
2022-07-01 12:00:30 +0530

    Add styled components

--- src/Components/GoalInputs.js
+++ src/Components/GoalInputs.js
@@ -1,7 +1,63 @@
 import React, { useState } from "react";
-import './GoalInputs.css';
+import styled from 'styled-components';
 
 function GoalInputs(props) {
+    const Button = styled.button`
+        font: inherit;
+        padding: 0.5rem 1.5rem;
+        border: 2px solid #2563eb;
+        color: white;
+        background: #2563eb;
+        box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+        cursor: pointer;
+    
+        &:focus {
+        outline: none;
+        }
+    
+        &:hover,
+        &:active {
+        background: #3b82f6;
+        border-color: #3b82f6;
+        box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+        }
+    `;
+
+    const FormControl = styled.div`
+        margin: 0.5rem 0;
+    
+        & label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+        font-size: x-large;
+        }
+    
+        & input {
+        display: block;
+        width: 100%;
+        height: 2rem;
+        border: 2px solid #bfdbfe;
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+        }
+    
+        & input:focus {
+        outline: none;
+        background: #bfdbfe;
+        border-color: #2563eb;
+        }
+    
+        &.invalid input {
+        border-color: #dc2626;
+        background-color: #fac0c0;
+        }
+    
+        &.invalid label {
+        color: #dc2626;
+        }
+    `;
     const [enteredGoalInput, setEnteredGoalInput] = useState('');
     const [isValid, setIsValid] = useState(true);
 
@@ -24,15 +80,11 @@ function GoalInputs(props) {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label style={{color: !isValid? '#dc2626' : 'black'}}>Goal</label>
-                <input type="text" value={enteredGoalInput} onChange={goalInputChangeHandler} 
-                       style={{borderColor: !isValid? '#dc2626' : '#bfdbfe', 
-                       backgroundColor: !isValid? '#fca5a5' : 'transparent'}} 
-                       placeholder='write your goal here'
-                />
-            </div>
-            <button type="submit" className="button">Add Goal</button>
+            <FormControl className={!isValid && 'invalid'}>
+                <label>Goal</label>
+                <input type="text" value={enteredGoalInput} onChange={goalInputChangeHandler} placeholder='write your goal here'/>
+            </FormControl>
+            <Button type="submit">Add Goal</Button>
         </form>
     )
 }
