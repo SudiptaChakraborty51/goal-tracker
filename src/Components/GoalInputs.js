ca2fe9f6d8e2c40c45702b76d8d0c8117db7b06b
Sudipta <hisudipta26@gmail.com>
2022-07-01 12:46:15 +0530

    added styled components with dynamic props

--- src/Components/GoalInputs.js
+++ src/Components/GoalInputs.js
@@ -1,63 +1,57 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
 
-function GoalInputs(props) {
-    const Button = styled.button`
-        font: inherit;
-        padding: 0.5rem 1.5rem;
-        border: 2px solid #2563eb;
-        color: white;
-        background: #2563eb;
-        box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-        cursor: pointer;
-    
-        &:focus {
-        outline: none;
-        }
-    
-        &:hover,
-        &:active {
-        background: #3b82f6;
-        border-color: #3b82f6;
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-        }
-    `;
+const Button = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 2px solid #2563eb;
+    color: white;
+    background: #2563eb;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
 
-    const FormControl = styled.div`
-        margin: 0.5rem 0;
-    
-        & label {
-        font-weight: bold;
-        display: block;
-        margin-bottom: 0.5rem;
-        font-size: x-large;
-        }
-    
-        & input {
-        display: block;
-        width: 100%;
-        height: 2rem;
-        border: 2px solid #bfdbfe;
-        font: inherit;
-        line-height: 1.5rem;
-        padding: 0 0.25rem;
-        }
-    
-        & input:focus {
-        outline: none;
-        background: #bfdbfe;
-        border-color: #2563eb;
-        }
-    
-        &.invalid input {
-        border-color: #dc2626;
-        background-color: #fac0c0;
-        }
-    
-        &.invalid label {
-        color: #dc2626;
-        }
-    `;
+    &:focus {
+    outline: none;
+    }
+
+    &:hover,
+    &:active {
+    background: #3b82f6;
+    border-color: #3b82f6;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    }
+`;
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+    & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: x-large;
+    color: ${props=>props.invalid? '#dc2626' : 'black'};
+    }
+
+    & input {
+    display: block;
+    width: 100%;
+    height: 2rem;
+    border: 2px solid ${props=>props.invalid? '#dc2626' : '#bfdbfe'};
+    background-color: ${props=>props.invalid? '#fac0c0' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    }
+
+    & input:focus {
+    outline: none;
+    background: #bfdbfe;
+    border-color: #2563eb;
+    }
+`;
+
+function GoalInputs(props) {
     const [enteredGoalInput, setEnteredGoalInput] = useState('');
     const [isValid, setIsValid] = useState(true);
 
@@ -80,7 +74,7 @@ function GoalInputs(props) {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl className={!isValid && 'invalid'}>
+            <FormControl invalid={!isValid}>
                 <label>Goal</label>
                 <input type="text" value={enteredGoalInput} onChange={goalInputChangeHandler} placeholder='write your goal here'/>
             </FormControl>
